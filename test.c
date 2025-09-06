911611fa12c04debb4277f405238f036eb541e56
ngxx-fus <msnp@outlook.com.vn>
2025-09-06 17:24:22 +0700

    add --info arg

--- test.c
+++ test.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int args, char** arg){
     printf("Hi from Nguyen Thanh Phu\n");
+    if(strcmp(arg[1], "--info") == 0){
+        printf("Secret: Nguyen Thanh Phu was born in 3 Feb!\n");
+    }
     for(int i = 0; i < args; ++i)
         printf ("arg[%d]: %s\n", i, arg[i]);
 
