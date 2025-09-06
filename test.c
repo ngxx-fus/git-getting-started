53e1c2ea9f9c9f99d5461f1c3e5fc04e09c88b04
ngxx-fus <msnp@outlook.com.vn>
2025-09-06 17:20:31 +0700

    doi loi chao: NEW: Hi from Nguyen Thanh Phu

--- test.c
+++ test.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(int args, char** arg){
-    printf("hello world\n");
+    printf("Hi from Nguyen Thanh Phu\n");
     for(int i = 0; i < args; ++i)
         printf ("arg[%d]: %s\n", i, arg[i]);
+
 }
