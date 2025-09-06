56fc7692fb30f5de73f74c5ad461a1e373faca0c
ngxx-fus <msnp@outlook.com.vn>
2025-09-06 18:12:04 +0700

    test.c has changed

--- test.c
+++ test.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-
-///
+const char* argList[] = {
+    "--info",
+    "--full-name",
+    "--change-name",
+}; 
+const int numParamOfArg[] = {
+    0,
+    0,
+    1
+};
 
 int main(int args, char** arg){
     printf("Hi from Nguyen Thanh Phu\n");
@@ -11,5 +19,22 @@ int main(int args, char** arg){
     }
     for(int i = 0; i < args; ++i)
         printf ("arg[%d]: %s\n", i, arg[i]);
-
+    for(int i = 0; i < args; ++i){
+        for(int j = 0; j < sizeof(numParamOfArg)/sizeof(int); ++j){
+            if(strcmp(arg[i], argList[j]) == 0){
+                switch(j){
+                    case 0:
+                        printf("Secret: Phu was born in 3 Feb!\n");
+                        break;
+                    case 1:
+                        printf("Name: Nguyen Thanh Phu!\n");
+                        break;
+                    case 2:
+                        if (i+1 == args) fprintf(stderr, "Not enough arg!\n");
+                        else printf("Send changed name request! (new name: %s)\n", arg[++i]);
+                        break;
+                }
+            }
+        }
+    }
 }
